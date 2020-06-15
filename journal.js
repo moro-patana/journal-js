1b1b48ac9f860dc35997053ee1ce197d7ecd8f86
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-06-15 12:03:37 +0300

    I Used if condition and for loop

--- journal.js
+++ journal.js
@@ -1,27 +1,41 @@
  console.log('it works');
-let JournalEntry = [
+const journal = [
   {
-    "title": "The horizon",
-    "content": "Beautiful evening with wonderful colors at the end of the horizon"
+    title: "The horizon",
+    content: "Beautiful evening with wonderful colors at the end of the horizon"
   },
   {
-    "title": "A little frog",
-    "content": "I found a poor little frog outside of my room. It was so cute."
+    title: "A little frog",
+    content: "I found a poor little frog outside of my room. It was so cute."
   },
   {
-    "title": "Mother's Day",
-    "content": "We were celebrating mother's day. It was on 7 of june 2020."
+    title: "Mother's Day",
+    content: "We were celebrating mother's day. It was on 7 of june 2020."
   }
-]
+];
+
+// First prompt for showing all the entries//
+
 let journalList = Number(prompt("Welcome to my Onja journal!\nEnter 1 for listing all entries"));
-for (let i = 0; i < JournalEntry.length; i++) { 
-    console.log (JournalEntry[i]);
+if (journalList === 1) {
+for (let i = 0; i < journal.length; i++) { 
+    console.log(journal[i]);
   }
-  let newJournalList = Number(prompt("Do you want to create new journal entry? (Press 2)")); 
-  let newObject = {"title": "", "content": ""};
-  JournalEntry.push(newObject);
+}
+// Second prompt ask user to create new journal and give instruction//
 
+  let newJournal = Number(prompt("Do you want to create new journal entry? (Press 2)")); 
+if (newJournal === 2) {
   let newJournalTitle = Number(prompt("Enter the title"));
-  let newJournlContent = Number(prompt("Enter new content"));
-  let journalQuit = Number(prompt("Press 3 to quit"));
+  let newJournalContent = Number(prompt("Enter new content"));
+  const newEntry = {title: newJournalTitle, content: newJournalContent};
+  journal.push(newEntry);
+  }
+// Last prompt if the user want to quit//
 
+let journalQuit = Number(prompt("Press 3 to quit"));
+let num = 0;
+while (num < 3) {
+  num++;
+}
+console.log(num);
